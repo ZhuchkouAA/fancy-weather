1662c59c28e66e11fa5b8401ed2b97ab3468705e
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-06-17 10:32:11 +0300

    fix: fix volume changes

--- src/js/speechRecognition.js
+++ src/js/speechRecognition.js
@@ -1,21 +1,21 @@
-import tellWeather from './voice';
-import search from './search';
-import changeBackgroundImage from './changeBackground';
+import tellWeather from "./voice";
+import search from "./search";
+import changeBackgroundImage from "./changeBackground";
 
 const recognizer = new webkitSpeechRecognition();
 recognizer.interimResults = true;
 
 function voiceUpVision() {
-  document.querySelector('.voice').classList.add('volume-up');
+  document.querySelector(".voice").classList.add("volume-up");
   setTimeout(() => {
-    document.querySelector('.voice').classList.remove('volume-up');
+    document.querySelector(".voice").classList.remove("volume-up");
   }, 1000);
 }
 
 function voiceDownVision() {
-  document.querySelector('.voice').classList.add('volume-down');
+  document.querySelector(".voice").classList.add("volume-down");
   setTimeout(() => {
-    document.querySelector('.voice').classList.remove('volume-down');
+    document.querySelector(".voice").classList.remove("volume-down");
   }, 1000);
 }
 
@@ -23,68 +23,70 @@ function voiceListenerResult(event) {
   const result = event.results[event.resultIndex];
   if (result.isFinal) {
     switch (result[0].transcript) {
-      case 'weather':
+      case "weather":
         tellWeather();
         break;
-      case 'погода':
+      case "погода":
         tellWeather();
         break;
-      case 'louder':
+      case "louder":
         if (window.myObj.volume < 1) {
-          window.myObj.volume += 0.1;
+          window.myObj.volume += 0.25;
         }
         voiceUpVision();
         break;
-      case 'громче':
+      case "громче":
         if (window.myObj.volume < 1) {
-          window.myObj.volume += 0.1;
+          window.myObj.volume += 0.25;
         }
         voiceUpVision();
         break;
-      case 'quieter':
+      case "quieter":
         if (window.myObj.volume > 0) {
-          window.myObj.volume -= 0.1;
+          window.myObj.volume -= 0.25;
         }
         voiceDownVision();
         break;
-      case 'тише':
+      case "тише":
         if (window.myObj.volume > 0) {
-          window.myObj.volume -= 0.1;
+          window.myObj.volume -= 0.25;
         }
         voiceDownVision();
         break;
-      case 'background':
+      case "background":
         changeBackgroundImage();
         break;
-      case 'фон':
+      case "фон":
         changeBackgroundImage();
         break;
       default:
-        document.querySelector('.input-search').value = result[0].transcript;
+        document.querySelector(".input-search").value = result[0].transcript;
         search();
     }
-    document.querySelector('.btn-microphone').classList.remove('btn-active');
-    document.querySelector('.input-search').value = '';
+    document.querySelector(".btn-microphone").classList.remove("btn-active");
+    document.querySelector(".input-search").value = "";
   } else {
-    document.querySelector('.input-search').value = result[0].transcript;
+    document.querySelector(".input-search").value = result[0].transcript;
   }
 }
 
 function voiceListener(e) {
   e.preventDefault();
-  const button = document.querySelector('.btn-microphone');
-  if (button.classList.contains('btn-active')) {
+  const button = document.querySelector(".btn-microphone");
+  if (button.classList.contains("btn-active")) {
     recognizer.stop();
-    button.classList.remove('btn-active');
-    document.querySelector('.input-search').value = '';
+    button.classList.remove("btn-active");
+    document.querySelector(".input-search").value = "";
   } else {
-    button.classList.add('btn-active');
-    const myObj = JSON.parse(localStorage.getItem('myObj'));
-    if (myObj.lang === 0) recognizer.lang = 'en-US';
-    if (myObj.lang > 0) recognizer.lang = 'ru';
+    button.classList.add("btn-active");
+    const myObj = JSON.parse(localStorage.getItem("myObj"));
+    if (myObj.lang === 0) recognizer.lang = "en-US";
+    if (myObj.lang > 0) recognizer.lang = "ru";
     recognizer.onresult = voiceListenerResult;
     recognizer.start();
   }
 }
 
-document.querySelector('.btn-microphone').addEventListener('click', voiceListener);
+document
+  .querySelector(".btn-microphone")
+  .addEventListener("click", voiceListener);
